9c44a71f02079d7c79a91a79b1b85726341c11dd
Diana Lopez <me@diana.nu>
2022-05-21 14:40:14 -0600

    change to ol list instead of table

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,57 +1,51 @@
 <template>
-  <div>
-    <header class="page-header">
+  <div class="section">
+    <header class="container has-text-centered">
       <p>🐋</p>
       <h1>Moby Dick.</h1>
+      <h2>Top 100 Words.</h2>
     </header>
-    <table class="top-words">
-      <caption>Top 100 Words</caption>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Word</th>
-          <th>Instances</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>100</td>
-          <td>whale</td>
-          <td>100</td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="loading">Loading...</div>
+    <div v-else class="container">
+      <ol>
+        <li :key="i" v-for="(entry, i) in topWords">
+          <span class="word">{{ entry.word }}</span>
+          <span class="count">{{ entry.count }}</span>
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      topWords: [],
+      loading: true
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3001/api/words")
+    .then(response => response.json())
+    .then(words => {
+      for (let word of words) {
+        this.topWords.push(word);
+      }
+      this.loading = false;
+    })
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.page-header {
-  text-align: center;
+ol {
+  list-style: none;
+  padding-left: 0;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1em;
 }
-.top-words {
-  width: 100%;
-  border-color: #E1E1E1;
-  border-style: solid;
-  border-width: 1px 1px 0 1px;
-  width: 50%;
-  margin: 0 auto;
-}
-th,
-td {
-  padding: 12px 15px;
-  text-align: left;
-  border-bottom: 1px solid #E1E1E1; }
-th:first-child,
-td:first-child {
-  padding-left: 0; }
-th:last-child,
-td:last-child {
-  padding-right: 0; }
 </style>
