1e5c0aae22457521c6683c10e93bf89d0e83505c
Diana Lopez <me@diana.nu>
2022-05-21 13:14:10 -0600

    remove useless? proxy

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      },
-    }
-  }
+  transpileDependencies: true
 })
