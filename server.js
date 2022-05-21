fcc057f84b39fbf6adcb24c15edd2d979d87fd32
Diana Lopez <me@diana.nu>
2022-05-20 20:04:25 -0600

    refactored

--- server.js
+++ server.js
@@ -16,81 +16,60 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('src'));
 
-app.get('/', (req, res) => {
-  const stopWordsFile = fs.readFileSync("./static/stop-words.txt").toString().split(/\r?\n/).filter(n => n);
-  const book = fs.readFileSync("./static/mobydick.txt").toString().split(/\r?\n/).filter(n => n);
-  const firstSentence = "Call me Ishmael.";
-  const endOfBook = "End of Project Gutenberg’s Moby Dick; or The Whale, by Herman Melville";
+const getFileSegment = (file, startText, endText) => {
+  var fileSplit = fs.readFileSync(file).toString().split(/\r?\n/).filter(n => n);
+  let segment = [];
+  let segmentStart;
 
-  let stopwords = [];
-  let firstStopWord;
-  let firstBookLine;
+  for (let [i, value] of fileSplit.entries()) {
+    if (value.trim().startsWith(endText)) break;
+    if (value.trim().startsWith(startText)) {
+      segmentStart = i;
+    }
+    
+    if (segmentStart && i >= segmentStart) {
+      segment.push(value);
+    }
+  }
+  return segment.filter(n => n);
+}
+
+const sortObjByValue = (obj) => {
+  var keys = Object.keys(obj);
+  return keys.sort((a,b) => {
+    if (obj[a] > 1) {
+      return obj[b]-obj[a]
+    }
+  });
+}
+
+const top100WordsInText = (text) => {
   let paragraphWords = [];
   let wordCounts = {};
+  let stopwords = getFileSegment('./static/stop-words.txt', 'a', 'z');
 
-  const getWordUseCount = (array, search) => {
-    var count = array.filter((v, i) => i === search).length;
-    return count;
-  }
-
-  const sortWords = (obj) => {
-    var keys = Object.keys(obj);
-    return keys.sort((a,b) => {
-      if (obj[a] > 1) {
-        return obj[b]-obj[a]
+  for (let [i, paragraph] of text.entries()) {
+    paragraphWords = paragraph.match(/\b(\w+)\b/g);
+    paragraphWords.forEach((word) => {
+      word = word.trim().toLowerCase();
+      if (stopwords.includes(word)) return;
+      word = pluralize.singular(word);
+      
+      if (wordCounts[word]) {
+        wordCounts[word] = wordCounts[word] + 1;
+      } else {
+        wordCounts[word] = 1;
       }
-    });
+    })
   }
+  return sortObjByValue(wordCounts).slice(0, 100);
+}
 
-  for (let [i, word] of stopWordsFile.entries()) {
-      if (word == 'a') {
-      firstStopWord = i;
-    }
-    
-    if (firstStopWord && i >= firstStopWord) {
-      stopwords.push(word);
-    }
-  }
-
-  stopwords = stopwords.filter(n => n);
-  let ok;
-
-  for (let [i, paragraph] of book.entries()) {
-    if (paragraph.trim().includes(endOfBook)) break;
-
-    if (paragraph.trim().startsWith(firstSentence)) {
-      firstBookLine = i;
-    }
-    
-    if (firstBookLine && i >= firstBookLine) {
-      paragraphWords = paragraph.match(/\b(\w+)\b/g);
-      paragraphWords.forEach((word) => {
-        word = word.trim().toLowerCase();
-        if (stopwords.includes(word)) return;
-        word = pluralize.singular(word);
-        
-        if (wordCounts[word]) {
-          wordCounts[word] = wordCounts[word] + 1;
-        } else {
-          wordCounts[word] = 1;
-        }
-      })
-    }
-  }
-
-  let rows = '';
-
-  top100 = sortWords(wordCounts).slice(0, 100);
-  let i = 0;
-
-  top100.forEach((word) => {
-    ++i;
-    rows = rows + '<tr><td>'+i+'</td><td>'+word+'</td><td>'+wordCounts[word]+'</td></tr>';
-  })
-
-  res.send('<table>'+rows+'</table>');
+app.get('/words', (req, res) => {
+  const mobyDick = getFileSegment('./static/mobydick.txt', 'Call me Ishmael.', 'End of Project Gutenberg’s Moby Dick');
+  const top100WordsInMobyDick = top100WordsInText(mobyDick);
+  res.send(top100WordsInMobyDick);
 });
 
 app.listen(port, () => {
