ce93bae85a9585928fca131159c08810363e91d8
Diana Lopez <me@diana.nu>
2022-05-21 10:07:51 -0600

    ui start

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="page-header">
+      <p>🐋</p>
+      <h1>Moby Dick.</h1>
+    </header>
+    <table class="top-words">
+      <caption>Top 100 Words</caption>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Word</th>
+          <th>Instances</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>100</td>
+          <td>whale</td>
+          <td>100</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss" scoped>
+.page-header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+.top-words {
+  width: 100%;
+  border-color: #E1E1E1;
+  border-style: solid;
+  border-width: 1px 1px 0 1px;
+  width: 50%;
+  margin: 0 auto;
+}
+th,
+td {
+  padding: 12px 15px;
+  text-align: left;
+  border-bottom: 1px solid #E1E1E1; }
+th:first-child,
+td:first-child {
+  padding-left: 0; }
+th:last-child,
+td:last-child {
+  padding-right: 0; }
 </style>
