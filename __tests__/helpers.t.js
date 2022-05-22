5190495d18e85a72d004f975aaaa291e656272e1
Diana Lopez <me@diana.nu>
2022-05-22 12:46:12 -0600

    organize tests

---
+++ __tests__/helpers.t.js
@@ -0,0 +1,22 @@
+import { getWords } from '../server/helpers.js';
+
+describe('Get words API test', function () {
+
+    test('returns an array', () => {
+        const req = {  };
+
+        const res = { text: '',
+            send: function(input) { this.text = input } 
+        };
+
+        const words = getWords(req, res);
+        
+        const parseJson = () => {
+          var json = JSON.stringify(words);
+          return JSON.parse(json);
+        };
+  
+        expect(parseJson).not.toThrow();
+    });
+
+});
