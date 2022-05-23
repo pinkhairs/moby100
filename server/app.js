9bc3a1034c66ca7229d27b2c137d15bd05dce2fb
Diana Lopez <me@diana.nu>
2022-05-22 22:20:11 -0600

    server abstracted from app code

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3001;
 
 const helpers = require('./helpers');
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const app = require('./app.js');
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on the port ${PORT}`);
 });
+
+module.exports = app;
